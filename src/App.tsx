77c7b95c9248735707de2f5f3341bee9d98b4980
Anton Penev <penevmx@gmail.com>
2023-07-29 17:00:51 +0300

    feat: create custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React, { FC, ReactElement } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import {customTheme} from './theme/customTheme'
+import { CssBaseline } from '@mui/material'
+
+
 const App: FC = (): ReactElement => {
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline/>
+      <h1>asd</h1>
+    </ThemeProvider>
+    
   )
 }
 
